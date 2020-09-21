d8e815db8cdabc668cfce528360f45f388f42d22
Peter <schmidt.multimedia@gmail.com>
2020-09-21 11:04:00 +0200

    update

--- challenge_02.js
+++ challenge_02.js
@@ -7,7 +7,7 @@ Geben über eine Prompt-Anforderung ein Alter ein
 Geben Sie in Abhängigkeit vom angegebenen Alter 
 folgende Getränkeauswahl in die Konsole aus aus :
 
-Alter 0-5 // Milch
+Alter 0-5 // Milch  --> age > 0 AND age < 6
 Alter 6-12 // Saft
 Alter 13-17 // Cola
 18 + // Wein
@@ -15,4 +15,25 @@ Alter 13-17 // Cola
 */
 
 /* Getränke-Challenge */
+
+let firstName = prompt("Bitte Namen eingeben:");
+let age =  parseInt(prompt("Bitte Alter eingeben:")); // "2" --> 2
+
+switch (true) {
+    case (age >= 0) && (age <= 5):
+        console.log(firstName + " trinkt Milch");
+        break;
+    case (age >= 6) && (age <= 12):
+        console.log(firstName + " trinkt Saft");
+        break;
+    case (age >= 13) && (age <= 17):
+        console.log(firstName + " trinkt Cola");
+        break;
+        default:
+        console.log(firstName + " trinkt Wein");   
+        break;
+}
+
+// Kritik: keine G.-Auswahl, Plausibilität der Eingaben
+
 
